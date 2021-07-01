5965fede9f3ac2a724ed7647910cfe4ea24bd590
valenteemanuele <85895875+valenteemanuele@users.noreply.github.com>
2021-07-01 09:37:29 +0200

    Delete Bash directory

--- Bash/ShellExpansion/expansions.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-first=${@:1:1}
-second=${@:2:1}
-third=${@:3:1}
-RESULT=$((first*(second+third)))
-echo project-$RESULT/src/{index.js,util.js} project-$RESULT/dest/{index.js,util.js} project-$RESULT/test/{index.js,util.js}
--- Bash/array/arrays.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-array=${@:2:2}
-
-arrayfinal=(I am ${array[*]} and ${@:4:1})
-echo ${arrayfinal[*]}
